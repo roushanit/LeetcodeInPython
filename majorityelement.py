4db5b2dc5c96007f30e7ec7decf64d6d97acc79e
roushanit <roushansingh1992@gmail.com>
2026-04-11 18:14:24 +0530

    Majority element in array in python code

---
+++ majorityelement.py
@@ -0,0 +1,13 @@
+def majorityElement(nums):
+    candidate = None
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+        
+        count += (1 if num == candidate else -1)
+
+    return candidate
+    
+print(majorityElement([1, 2, 1])) 
