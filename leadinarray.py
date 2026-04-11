2384e528a61a27029a02795e2bba8baf391c1d1a
roushanit <roushansingh1992@gmail.com>
2026-04-11 18:16:49 +0530

    Leaders in an array in python code

---
+++ leadinarray.py
@@ -0,0 +1,16 @@
+def leaders(arr):
+    n = len(arr)
+    result = []
+    
+    max_so_far = arr[-1]
+    result.append(max_so_far)
+    
+    # Traverse from right to left
+    for i in range(n-2, -1, -1):
+        if arr[i] >= max_so_far:
+            max_so_far = arr[i]
+            result.append(arr[i])
+    
+    # Reverse to maintain order
+    return result[::-1]
+print(leaders([6,7,3,4,5,2]))
